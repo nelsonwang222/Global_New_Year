2f1bfd42487d7439c6a32ff05f34c9e11b651b3b
nelsonwang222 <bakerwql@gmail.com>
2026-01-01 16:53:47 +0800

    feat: Set base public path to `/Global_New_Year/` in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Global_New_Year/',
       server: {
         port: 3000,
         host: '0.0.0.0',
